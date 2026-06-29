f456e2c0f1666dbcdc01454dbd56845fe38e55e2
Hrishita <hrishita604@gmail.com>
2026-06-29 18:12:23 +0530

    Day 21 - Implemented Angular forms and two-way data binding

--- EmployeeManagementUI/src/app/components/login/login.ts
+++ EmployeeManagementUI/src/app/components/login/login.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  username ="";
+  password ="";
+  login(){
+    console.log(this.username);
+    console.log(this.password);
+  }
+}
